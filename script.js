c7d08fe790263a838c1277dcc8e3f67bb8b0c093
Unmesh Bari <unmeshbari5@gmail.com>
2025-11-09 23:30:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,24 @@
 // Complete the js code
 function Car(make, model) {
-	this.make = make 
-	this.model = model
+  this.make = make;
+  this.model = model;
 }
 
-Car.prototype.getMakeModel = ()=>{
-	return `${this.make} ${this.model}`
-}
-
+Car.prototype.getMakeModel = function() {
+  return `${this.make} ${this.model}`;
+};
 
 function SportsCar(make, model, topSpeed) {
-	Car.call(this,make, model)
-	this.topSpeed = topSpeed
+  Car.call(this, make, model);
+  this.topSpeed = topSpeed;
 }
 
-SportsCar.prototype = Object.create(Car.prototype)
+SportsCar.prototype = Object.create(Car.prototype);
 SportsCar.prototype.constructor = SportsCar;
 
-SportsCar.prototype.getTopSpeed = ()=>{
-	return this.topSpeed
-}
+SportsCar.prototype.getTopSpeed = function() {
+  return this.topSpeed;
+};
 
 // Do not change the code below
 window.Car = Car;
